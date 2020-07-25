0f32db034338f00d066f4312e2224b511e180f98
EldinKajdic <eldinkajdic@hotmail.com>
2020-07-26 00:24:37 +0200

    Added pagination and search form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useFetchJobs from './useFetchJobs';
-import { Container } from 'react-bootstrap';
+import { Container, Spinner } from 'react-bootstrap';
+import Job from './Job';
+import JobsPagination from './JobsPagination';
+import SearchForm from './SearchForm';
 
 function App() {
-  const { jobs, loading, error } = useFetchJobs();
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
+
+  function handleParamChange(e) {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1);
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value };
+    });
+  }
+
   return (
-    <Container>
-      {loading && <h1>Loading...</h1>}
+    <Container className="my-4">
+      <h1>Developer jobs</h1>
+      <h2 className="mb-4">Search for developer jobs all around the world</h2>
+      <SearchForm params={params} onParamChange={handleParamChange} />
+      <JobsPagination
+        page={page}
+        setPage={setPage}
+        hasNextPage={hasNextPage}
+        display={jobs.length > 0 && !loading}
+      />
+      {loading && (
+        <div className="text-center">
+          <h1>Loading...</h1>
+          <Spinner animation="border" role="status">
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        </div>
+      )}
       {error && <h1>Error. Try refreshing.</h1>}
-      <h1>{jobs.length}</h1>
+      {jobs.map((job) => {
+        return <Job key={job.id} job={job} />;
+      })}
+      <JobsPagination
+        page={page}
+        setPage={setPage}
+        hasNextPage={hasNextPage}
+        loading={loading}
+        display={jobs.length > 0 && !loading}
+      />
     </Container>
   );
 }
